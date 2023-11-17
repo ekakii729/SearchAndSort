7ba2ff707e9338d102929cc627f81f76d354d65a
Abhay Manoj <sanicthecheese@gmail.com>
2023-11-16 21:14:57 -0500

    overloaded some methods

--- src/com/ekakii729/searchAndSort/SortSearchLibrary.java
+++ src/com/ekakii729/searchAndSort/SortSearchLibrary.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class SortSearchLibrary {
 
-    public static void selectionSort(int[] nums) {
+    public void selectionSort(int[] nums) {
         final int ARRAY_INDEX_COUNT = nums.length; // the index count of the array
         for (int i = 0; i < ARRAY_INDEX_COUNT - 1; i++) {
             int minimumIndex = i; // the minimum index
@@ -13,14 +13,30 @@ public class SortSearchLibrary {
         }
     }
 
-    public static void quickSort(int[] nums, int lowestIndex, int highestIndex) {
+    public void selectionSort(String[] strings) {
+        final int ARRAY_INDEX_COUNT = strings.length; // the index count of the array
+        for (int i = 0; i < ARRAY_INDEX_COUNT - 1; i++) {
+            int minimumIndex = i; // the minimum index
+            for (int j = i + 1; j < ARRAY_INDEX_COUNT; j++) if (strings[j].compareTo(strings[minimumIndex]) < 0) minimumIndex = j;
+            swap(strings, minimumIndex, i);
+        }
+    }
+
+    public void quickSort(int[] nums, int lowestIndex, int highestIndex) {
         if (lowestIndex >= highestIndex) return;
         int partition = partition(nums, lowestIndex, highestIndex); // the middle point of the array
         quickSort(nums, lowestIndex, partition - 1);
         quickSort(nums, partition + 1, highestIndex);
     }
 
-    private static int partition(int[] nums, int lowestIndex, int highestIndex) {
+    public void quickSort(String[] strings, int lowestIndex, int highestIndex) {
+        if (lowestIndex >= highestIndex) return;
+        int partition = partition(strings, lowestIndex, highestIndex); // the middle point of the array
+        quickSort(strings, lowestIndex, partition - 1);
+        quickSort(strings, partition + 1, highestIndex);
+    }
+
+    private int partition(int[] nums, int lowestIndex, int highestIndex) {
         int pivot = nums[highestIndex]; // the pivot point
         int smallestElement = lowestIndex - 1; // the smallest element in the array
         for (int j = lowestIndex; j <= highestIndex; j++) {
@@ -32,13 +48,31 @@ public class SortSearchLibrary {
         return (smallestElement + 1);
     }
 
-    private static void swap(int[] nums, int i, int j) {
+    private int partition(String[] strings, int lowestIndex, int highestIndex) {
+        String pivot = strings[highestIndex]; // the pivot point
+        int smallestElement = lowestIndex - 1; // the smallest element in the array
+        for (int j = lowestIndex; j <= highestIndex; j++) {
+            if (strings[j].compareTo(pivot) < 0) {
+                smallestElement++;
+                swap(strings, smallestElement, j);
+            }
+        } swap(strings, smallestElement + 1, highestIndex);
+        return (smallestElement + 1);
+    }
+
+    private void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
     }
 
-    public static void printArray(int[] nums) {
+    private void swap(String[] strings, int i, int j) {
+        String temp = strings[i];
+        strings[i] = strings[j];
+        strings[j] = temp;
+    }
+
+    public void printArray(int[] nums) {
         for (int num : nums) System.out.println(num);
     }
 
